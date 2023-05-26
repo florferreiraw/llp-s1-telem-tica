503032d299f66426bd18d4fa29ac6aea6a4a8bef
Lorena Ferreira <127799245+florferreiraw@users.noreply.github.com>
2023-05-26 16:09:37 -0300

    Create atividade_03.c

---
+++ atividades/atividade03/atividade_03.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// estrutura
+struct Dados {
+    char nome[30];
+    int idade;
+    int altura;  };
+
+int main() {
+    // Agora, crio uma instância da estrutura
+    struct Dados dados;
+
+    // Recebendo os dados 
+    printf("Digite o nome:\n");
+    fgets(dados.nome, sizeof(dados.nome), stdin);
+
+    printf("Digite a idade:\n");
+    scanf("%d", &dados.idade);
+  
+    printf("Digite a sua altura:\n");
+    scanf("%d", &dados.altura);
+
+    // Abrindo o arquivo para escrever os dados
+    FILE *arquivo = fopen("dados.txt", "w");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.");
+        return 1; // Encerrar o programa com erro
+    }
+
+    // Gravando a estrutura no arquivo
+    fprintf(arquivo, "%s%d", dados.nome, dados.idade, dados.altura);
+
+    // Fechando o arquivo
+    fclose(arquivo);
+
+    printf("Dados gravados no arquivo com sucesso.");
+
+    return 0;
+}
