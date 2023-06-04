79820626016f4a084985f7fa05a3e3124bfafd27
Lorena Ferreira <127799245+florferreiraw@users.noreply.github.com>
2023-06-04 16:35:50 -0300

    Create atividade04_ITEM'C'.c

---
+++ atividades/Atividade04/atividade04_ITEM'C'.c
@@ -0,0 +1,39 @@
+//Usando switch, escreva um programa que leia um inteiro entre 1 e 7 e imprima o dia da semana correspondente a este numero. Isto é, domingo se 1, segunda-feira se 2, e assim por diante.
+
+#include <stdio.h>
+
+int main() {
+    int dia;
+
+    printf("Digite um número entre 1 e 7: ");
+    scanf("%d", &dia);
+
+    switch (dia) {
+        case 1:
+            printf("Domingo\n");
+            break;
+        case 2:
+            printf("Segunda-feira\n");
+            break;
+        case 3:
+            printf("Terça-feira\n");
+            break;
+        case 4:
+            printf("Quarta-feira\n");
+            break;
+        case 5:
+            printf("Quinta-feira\n");
+            break;
+        case 6:
+            printf("Sexta-feira\n");
+            break;
+        case 7:
+            printf("Sábado\n");
+            break;
+        default:
+            printf("Número inválido! Digite um número entre 1 e 7.\n");
+            break;
+    }
+
+    return 0;
+}
