ff69f6845e3f846240a91e49151fca10f7244e4e
Lorena Ferreira <127799245+florferreiraw@users.noreply.github.com>
2023-06-07 12:44:52 -0300

    Create atividade05_Q6.c
    questão 6 concluída

---
+++ atividades/atividade05/atividade05_Q6.c
@@ -0,0 +1,36 @@
+//6) Faça um programa em c que receba do usuário um arquivo texto e mostre na tela quantas vezes cada letra do alfabeto aparece dentro do arquivo
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *arquivo;
+    char nomeArquivo[200];
+    char caractere;
+    int contagem[26] = {0}; 
+
+    printf("Digite o nome do arquivo: ");
+    scanf("%s", nomeArquivo);
+
+    arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo, insira um arquivo válido.\n");
+        return 1;
+    }
+
+    while ((caractere = fgetc(arquivo)) != EOF) {
+        if (isalpha(caractere)) { //ver se o caractere é uma letra
+            caractere = toupper(caractere);
+            contagem[caractere - 'A']++; // Incrementa a contagem da letra correspondente
+        }
+    }
+
+    fclose(arquivo);
+
+    printf("Contagem de letras no arquivo:\n");
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'A' + i, contagem[i]);
+    }
+
+    return 0;
+}
